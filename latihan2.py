fd5d24696826dabd4f8366f235b9b656a17cc071
ArRahmaan17 <ardrah17@gmail.com>
2020-06-25 05:34:55 +0700

    Validate Password

---
+++ latihan2.py
@@ -0,0 +1,14 @@
+print("Author = Ardhi rahmaan")
+data = input("masukan Password ")
+
+for i in data:
+    password = "contohpass"
+    j = 7
+    if j >= len(data):
+        print("maaf password minimal 8 karakter, contoh:",password)
+        data = input("masukan kata ")
+
+print("password telah di simpan!")
+print("program validasi password 8 karakter di python")
+
+
